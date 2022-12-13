98be24cb6937b27c083ae5a503bc478c5a62363c
MukhtairAhmed <114793154+MukhtairAhmed@users.noreply.github.com>
2022-12-13 10:15:56 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,467 @@
+// alert-box 
+// var a="Javascript";
+
+// if(a ==a){
+//   var b=  alert("Error:Please enter a valid password");
+// }
+
+
+// var a =10;
+// var b=10;
+
+// if(a ==b){
+//     alert("Welcome To Js Land...")
+// alert("Happy coding")
+// }
+
+
+// var a =10;
+// var b=10;
+
+// if(a===b){
+//     alert("WELCOME TO JS")
+// }
+
+
+
+// var a=20;
+// var b =20;
+// if(a==b){
+//     alert("Hello.. I Can run js through my web browser console")
+// }
+
+
+
+// var a =10;
+// var b=10
+// if(a == b){
+//     alert("pervent this page from crating additional dailogs")
+// }
+
+
+
+
+
+// varaibles for string
+
+// var username="ALi";
+// var myname="Ahmed Soomro";
+
+// if(username==myname){
+// alert(username)
+
+// }else{
+//     alert(myname);
+// }
+
+
+
+// var str=18;
+// var a=15;
+
+// if(str<a || str>a){
+//     alert(str);
+// }else{
+//     alert(a);
+// }
+
+
+// var certified ="Certified"
+// var galt="Certified Mobile Application Development"
+
+// if(certified>galt || certified==galt){
+//     alert(certified)
+// }else{
+//     alert(galt)
+// }
+
+
+// var useremail="example My Email Address Ahmed soomro253@gamil.com";
+// var b ="Galt email Address"
+
+// if(useremail<b || useremail>=b){
+//     alert(useremail)
+// }else{
+//     alert(b)
+// }
+
+
+// var store="---------^^^^^--------"
+
+// var full="none of these"
+
+// if(store<=full){
+//     alert(store)
+// }else{
+//     alert(full)
+// }
+
+
+
+
+// var a=1;
+// var b=1;
+
+// if(a==b){
+// document.write("Yah I can writ html Content through javascript")
+// }
+
+
+
+// var a ="Smart way to learn  javascript";
+// var b="javascript learning";
+
+// if(a<b){
+// alert("I  am  Trying  to  learn  from   the book   a  smarter  waye  to  learn  javascript ")
+ 
+// }
+
+
+// var a="pizza";
+// var b="galt";
+
+// if(a ==a){
+// alert("pizza                                                                            pizz                                                                            plz                                                                             pi                                                                              p")
+// }
+
+
+
+// varaible for number
+
+
+// for(var a=1;a<5;a++){
+//     var numb=prompt("enter the number")
+//      if(numb.length){
+//      if(numb>=5){
+//      alert("I am 15 Years Old")
+//   }
+//  }
+// }
+
+
+
+
+// for(var a=1;a<3;a++){
+// var inp=prompt("enter the value");
+// if(inp.length){
+// if(inp>3){
+// alert("You Have Visted This site 14 Times")
+
+// }else{
+//     alert("Noaone You have visited no site")
+// }
+// }
+// }
+
+
+
+
+// var currentdate =new Date();
+// var dob =new Date("17 april 2004");
+
+
+// currentdate.getDate();
+// dob.getFullYear();
+
+// var res=getDate.currentdate() - dob.getFullYear();
+// console.log(res)
+
+
+
+
+
+// for(var a;a==a;a++){
+// if(a==a){
+// document.write("Johen Doe Ordered S T-shirt on shaheenclothinstore")
+
+// }
+// }
+
+
+
+// varabile name: legal & IllEGAL
+
+// var a =(1,2,3);
+// var b=(1,2,3);
+// if(a==b){
+//     document.write(a)
+// }
+
+
+
+
+// var a=10;
+// var b=10;
+
+// if(a==b){
+//     document.write("APPLE BANANA ORANGE PINEAPPLE STRAWBERRY");
+// }
+
+
+
+
+// var fruit=1;
+// var IllEGAL=1;
+
+// if(1==1){
+//     document.write("cigerat,sharab,sharasi,baag,alchol");
+// }
+
+
+
+// var a=1;
+// var b=1;
+// var c=2;
+// var s=2;
+// var q=3;
+// var t=3;
+// if(a==b){
+//     document.write(" VARAIBLE NAME CAN BE ONLY Javascript FOR EXAMPLE 123<br/><br/>");
+// }if (c==s) {
+//     document.write("Varaible Begin  With Letter Asad,bilal,Nouman<br/><br/> ");
+// } if(q==t){
+//     document.write("VARABLE CAN BE MUST  SENSATIVE<br/><br/>")
+// }
+    
+
+
+
+
+// MATH EXPRESSIONS
+
+
+
+//  i=0
+//  while(i==0){
+//    inp1 = parseInt(prompt("Enter first Number"))
+//    inp2 =parseInt( prompt("Enter first Number"))
+//     op = prompt("enter Operation : +,-,*,/,%")
+//      if(op=="+"){
+//          alert("value of 1 : "+inp1+"\n"+"SUM OF  2 : "+inp2+"\n RESULT of : "+(inp1+inp2))
+//      }
+//      else if(op=="-"){
+//          alert("value of 1 : "+inp1+"\n"+"value of 2 : "+inp2+"\n Result is : "+(inp1-inp2))
+//      }
+//      else{
+//          alert("encorrect op")
+//      }
+
+//   i=prompt("Enter value for run again loop  0")
+// }
+
+
+
+
+
+// var a=1;
+
+// for(var a=1 ;a<5;a++){
+// var inp =prompt("Enter the Value");
+
+// if(inp.length){
+// if(inp > a){
+//     alert("Intail VALUE"+inp);
+
+// }
+// }
+// }
+
+
+
+
+
+
+// var b=1;
+
+// for(var a=6;a<=6;a++){
+// var num=prompt("ENTER THE VALUE");
+// if(num.length){
+// if(num == a){
+// alert("value of incerment"+ num)
+// }
+// }
+// }
+
+
+
+
+
+// var a=1;
+
+// while(a==1){
+// inp1=parseInt(prompt("ENTER THE NUMBER"))
+// inp2=parseInt(prompt("ENTER THE NUMBER"))
+// op=prompt("ENTER THE OPERRATOR");
+
+// if(op=="+"){
+//     alert("VALUE OF :"+inp1+"\n"+ "result of"+ inp2+"\n Value After Addition"+ (inp1+inp2));
+// }if(op=="-"){
+//     alert("VALUE OF :"+inp1+"\n"+ "result of"+ inp2+"\n RESULT"+ (inp1-inp2));
+// }
+// }
+
+
+
+
+// var a=12;
+
+// for(var a=12;a<=12;a++){
+// var num=prompt("ENTER THE VALUE");
+// if(num.length){
+// if(num == a){
+// alert("VALUE OF DECERMENT "+ num)
+// }
+// }
+// }
+
+
+
+
+
+// var a=1;
+// var b=2;
+
+// if(a==b){
+//     document.write("THE VALUE")
+// }else{
+//     document.write("THE REMAINDER 0")
+// }
+
+
+
+
+
+//  i=600;
+//  while(i==600){
+//    inp1 = parseInt(prompt("Enter first Number"))
+//    inp2 =parseInt( prompt("Enter first Number"))
+//     op = prompt("enter Operation : +,-,*,/,%")
+//      if(op=="*"){
+//          alert("value of 1 : "+inp1+"\n"+" TOTAL COST TO BUY  : "+inp2+"\n TICKET TO MOVIE PKR : "+(inp1*inp2))
+//      }
+//      else if(op=="-"){
+//          alert("value of 1 : "+inp1+"\n"+"value of 2 : "+inp2+"\n Result is : "+(inp1-inp2))
+//      }
+//      else{
+//          alert("encorrect op")
+//      }
+
+//   i=prompt("Enter value for run again loop  0")
+// }
+
+
+
+
+// var a = 2;
+// document.writeln("<br>"+a + " * 1 =" + a*1)
+// document.writeln("<br>"+a + " * 2 =" + a*2)
+// document.writeln("<br>"+a + " * 3 =" + a*3)
+// document.writeln("<br>"+a + " * 4 =" + a*4)
+// document.writeln("<br>"+a + " * 5 =" + a*5)
+// document.writeln("<br>"+a + " * 6 =" + a*6)
+// document.writeln("<br>"+a + " * 7 =" + a*7)
+// document.writeln("<br>"+a + " * 8 =" + a*8)
+// document.writeln("<br>"+a + " * 9 =" + a*9)
+// document.writeln("<br>"+a + " * 10 =" + a*10)
+
+
+// p1=650
+// p2=100
+// var q1 =  parseInt(prompt());
+// var q2 =  parseInt(prompt());
+
+// var sum = q1*p1  + q2*p2 ;
+
+// document.write("<br>"+sum+40);
+
+
+
+
+
+
+// var a =10;
+// var b=10;
+
+// if(a==b){
+// document.write("PRICE OF ITEM 1 is 650<br/>")
+
+// }if(a==b){
+// document.write("QUANTITY OF ITEM 1 IS 3<br/>")
+
+// }if(a==b){
+// document.write("PRICE OF ITEM 2 IS 100<br/>")
+
+// }
+// if(a==b){
+// document.write("QUANTITY OF ITEM 2 IS 7<br/>")
+
+// }if(a==b){
+//     document.write("Shipping change 100<br/>")
+// }if(a==b){
+//     document.write(" TOTAL OF 2750<br/>")
+// }
+
+
+
+
+
+// document.write("TOTAL MARKS :980<br/>");
+// document.write("OBTANIED MARKS :804<br/>");
+
+// i=980;
+
+// while(i==980){
+//   inp1 = parseInt(prompt("Enter first Number"))
+//   inp2 =parseInt( prompt("Enter first Number"))
+//    op = prompt("enter Operation : +,-,*,/,%")
+//     if(op=="*"){
+//         alert("value of 1 : "+inp1+"\n"+" PERCANTAGE  : "+inp2+"\n PERCANTAGE : "+(inp1*inp2))
+//     }
+//     else if(op=="-"){
+//         alert("value of 1 : "+inp1+"\n"+"value of 2 : "+inp2+"\n Result is : "+(inp1-inp2))
+//     }
+//     else{
+//         alert("encorrect op")
+//     }
+
+//  i=prompt("Enter value for run again loop  0")
+// }
+
+
+
+
+
+// var a =10;
+// var b=10;
+
+// if(a==b){
+// document.write("FAVOURITE SNACK :CHOCOLATE CHIP<br/>")
+
+// }if(a==b){
+// document.write("CURRENT AGE 15<br/>")
+
+// }if(a==b){
+// document.write("ESTIMITED MAXIMUN AGE 65<br/>")
+
+// }
+// if(a==b){
+// document.write("AMOUNT OF SNACK PER DAYS 3<br/>")
+
+// }if(a==b){
+//     document.write("YOU WILL NEED 150 CHOCOLATE CHIP TO LAST YOU UNTIL THE RIPE OLD AGE 65<br/>")
+// }
+
+
+
+
+var a=230;
+var b=78;
+
+if(a>b){
+document.write("TOTAL CURRENCY IN PKR 2000")
+
+
+}
+
+
+
